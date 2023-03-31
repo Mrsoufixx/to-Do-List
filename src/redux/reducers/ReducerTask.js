115cce8c419cbda639855ccfcc80b32db3433324
Mrsoufixx <korchi.soufiane@gmail.com>
2023-03-31 10:08:35 +0000

    add tailwind and redux folder

---
+++ src/redux/reducers/ReducerTask.js
@@ -0,0 +1,16 @@
+
+
+const initialise =[
+      {id:1,description:"create todo app with redux",isDone:false},
+      {id:2,description:"reveiller tôt",isDone:true}
+]
+
+
+export const ReducerTask = (props) => {
+  return (
+    <div>ReducerTask</div>
+  )
+}
+
+
+export default (ReducerTask)
